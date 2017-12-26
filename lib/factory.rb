3d79ae79022efc31af0d0604c0f46d4e4d70cace
andy <dp130291mav@gmail.com>
2017-12-27 00:09:51 +0200

    Complete test

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,53 +1,105 @@
 class Factory
-	def self.new *variables, &block
-		instance_class = Class.new do
 
-			attr_accessor *variables
+  def self.new *variables, &block
 
-			define_method :initialize do |*values|
+    class_name = if variables.first.is_a? String
+      unless variables.first.match(/^[A-Z]/)
+        raise(NameError, "#{variables.first} it must be a constant")
+      end
+      variables.shift
+    end
 
-				if values.length > variables.length
-					raise ArgumentError
-				end
+    example_class = Class.new do
 
-				values.each_with_index do |value, arg|
-					instance_variable_set("@#{variables[arg]}", value)
-				end
-			end
+    attr_accessor *variables
 
-			def iv_set
-				instance_variable_set("@#{variables[arg]}", value)
-			end
-			
-			define_method :[] do |arg|
-				if arg.instance_of? Integer
-					instance_variable_get("@#{variables[arg]}")
-				else
-					instance_variable_get("@#{arg}")
-				end
-			end
+      define_method :initialize do |*values|
+        if values.length > variables.length
+        raise ArgumentError
+        end
+        values.each_with_index do |value, arg|
+        instance_variable_set("@#{variables[arg]}", value)
+        end
+      end
 
-			define_method :[]= do |arg, value|
-				if arg.instance_of? Integer
-					iv_set
-				else
-					instance_variable_set("@#{arg}", value)
-				end
-			end
+      define_method :[] do |arg|
+        if arg.is_a? Numeric
+        raise IndexError unless instance_variables[arg]
+        instance_variable_get("@#{variables[arg]}")
+        else
+        raise NameError unless instance_variable_get("@#{arg}")
+        instance_variable_get("@#{arg}")
+        end
+      end
 
-			define_method :== do |other|
-				return false unless self.class == other.class 
-					variables.each do |var| 
-				return false unless instance_variable_get("@#{var}") == other.instance_variable_get("@#{var}")
-					end
-				return true
-			end   
+      define_method :[]= do |arg, value|
+        if arg.instance_of? Integer
+        raise IndexError unless instance_variables[arg]
+        instance_variable_set("@#{variables[arg]}", value)
+        else
+        raise NameError unless instance_variable_get("@#{arg}")
+        instance_variable_set("@#{arg}", value)
+        end
+      end      
 
-			class_eval(&block) if block_given?
+      define_method :== do |other|
+        return false unless self.class == other.class 
+        variables.map do |arg| 
+        return false unless instance_variable_get("@#{arg}") == other.instance_variable_get("@#{arg}")
+        end
+        return true
+      end   
 
+      define_method :members do
+        variables
+      end
 
-		end		
-	end
+      define_method :each do |&block|
+        values.each(&block)
+      end
+
+      define_method :each_pair do |&block|
+        to_h.each_pair(&block)
+      end
+
+      define_method :values do
+        members.map { |arg| instance_variable_get("@#{arg}") }
+      end     
+
+      define_method :values_at do |*arg|
+        arg.map do |i|
+        raise IndexError unless instance_variables[i]
+        to_a[i]
+        end
+      end
+
+      define_method :dig do |*key|
+        to_h.dig(*key)
+      end
+
+       define_method :to_h do
+        hash = Hash.new
+        variables.each_index do |i|
+        hash[:"#{variables[i]}"] = instance_variable_get("@#{variables[i]}")
+        end
+        hash
+      end
+
+      define_method :length do
+        members.length
+      end 
+
+      define_method :select do |&block|
+        to_a.select(&block)
+      end
+
+      alias_method :size, :length
+      alias_method :to_a, :values
+
+      class_eval(&block) if block_given?  
+    end 
+    class_name ? const_set(class_name, example_class) : example_class      
+  end
 end
 # --------------------------------------------------
 
--- lib/main.rb
+++ lib/main.rb
@@ -1,36 +1,53 @@
 require_relative 'factory'
 
-User = Factory.new :name, :age do
-	public
-	def say
-		puts "Hi all, I am #{self.name}"
-	 end
-end
+# User = Factory.new :name, :age do
+# 	public
+# 	def say
+# 		puts "Hi all, I am #{self.name}"
+# 	 end
+# end
 
-user = User.new "Den", "15"
-otherUser = User.new "Den", "15"
-puts user
-puts user.name
-puts user.age
-
-
-Customer = Factory.new(:name, :address) do
-	def greeting
-		"Hello #{name}!"
-	end
-end
-
-customer = Customer.new "Den", "15"
-puts customer
-puts customer.greeting
-
-puts user == otherUser ? 'equal' : 'not equal' 
-puts user == customer ? 'equal' : 'not equal' 
+# user = User.new "Den", "15"
+# otherUser = User.new "Den", "15"
+# puts user
+# puts user.name
+# puts user.age
 
 
-Factory.new('Customer', :name, :address)
+# Customer = Factory.new(:name, :address) do
+# 	def greeting
+# 		"Hello #{name}!"
+# 	end
+# end
 
-customer = Factory::Customer.new('Dave', '123 Main')
+# customer = Customer.new "Den", "15"
+# puts customer
+# puts customer.greeting
 
-puts customer.greeting
-puts customer.methods.grep(/greeting/)
+# puts user == otherUser ? 'equal' : 'not equal' 
+# puts user == customer ? 'equal' : 'not equal' 
+
+
+# Factory.new('Customer', :name, :address)
+
+# customer = Factory::Customer.new('Dave', '123 Main')
+
+# puts customer.greeting
+# puts customer.methods.grep(/greeting/)
+
+# Customer = Factory.new(:name, :address, :zip)
+# joe = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
+# joe.each {|x| puts(x) }
+
+# Lots = Struct.new(:a, :b, :c, :d, :e, :f)
+# l = Lots.new(11, 22, 33, 44, 55, 66)
+# l.select {|v| (v % 2).zero? }
+
+# puts l   
+
+Customer = Struct.new(:name, :address, :zip)
+joe = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
+
+# joe["name"] 
+# puts joe
+# joe[:name]
